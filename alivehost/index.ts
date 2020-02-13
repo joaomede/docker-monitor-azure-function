1b9ee9c0ee718a06217c5eda03a4020b6aa4f96c
Joao Medeiros <joaoserj@gmail.com>
2020-02-13 11:51:44 -0300

    init

---
+++ alivehost/index.ts
@@ -0,0 +1,74 @@
+import { AzureFunction, Context } from "@azure/functions"
+import Dockerode = require('dockerode')
+import nodemailer = require('nodemailer')
+
+const user = process.env.EMAIL
+const pass = process.env.PASSWORDSMTP
+const targetEmail = process.env.TARGETEMAIL
+const dockerHost = process.env.DOCKERHOST
+const dockerPort = process.env.DOCKERPORT
+
+const mail = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    auth: { user, pass },
+    tls: {
+        rejectUnauthorized: false
+    }
+})
+
+
+function createInstance(): Dockerode {
+    return new Dockerode({
+        socketPath: '',
+        host: dockerHost,
+        port: dockerPort
+    })
+}
+
+async function sendEmail(email: string, message: string, errorMessage: string, context: Context): Promise<void> {
+    try {
+        await mail.sendMail({
+            to: email,
+            from: `"Docker Checker" <${user}>`,
+            subject: 'Docker Alive Message',
+            text: message,
+            html: `<div>${message}<div>`
+        })
+    } catch (error) {
+        if (error) {
+            context.log(error)
+            throw new Error(errorMessage)
+        }
+    }
+}
+
+const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
+    // var timeStamp = new Date().toISOString();
+    const dockerode = createInstance()
+
+    try {
+        const testping = await dockerode.ping()
+        context.log(`O Docker do servidor Azure está ${testping.toString()}`)
+        await sendEmail(
+            targetEmail,
+            `O Docker do servidor Azure está ${testping.toString()}`,
+            'Erro ao tentar enviar email',
+            context
+        )
+    } catch (error) {
+        context.log(`Erro no servidor Docker ${error.message}`)
+        await sendEmail(
+            targetEmail,
+            `Erro no servidor Docker ${error.message}`,
+            'Erro ao tentar enviar email',
+            context
+        )
+    }
+    // if (myTimer.IsPastDue) {
+    //     context.log('Timer function is running late!');
+    // }
+    // context.log('Timer trigger function ran!', timeStamp);
+};
+
+export default timerTrigger;
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Docker Monitor - Azure Function
+
+## Description
+Simple simple routine to check the life of a docker server via api and send an email notifying its status.
